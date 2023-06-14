f607365282b0b64dd9c7003c5fffb2f537aef812
Wen Hanlin <2802420396@qq.com>
2023-06-14 19:52:01 +0800

    更新 ProxyFixer.cpp

--- ProxyFixer.cpp
+++ ProxyFixer.cpp
@@ -1,13 +1,7 @@
 #include <cstdlib>
-#include <Windows.h> 
-
+#include <Windows.h>
 
 void disableSystemProxy() {
-
-    // Hide console window
-    HWND hWnd = GetConsoleWindow();
-    ShowWindow(hWnd, SW_HIDE);
-    // ShowWindow(hWnd, SW_SHOW);
     std::system("reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\" /v ProxyEnable /t REG_DWORD /d 0 /f");
 }
 
